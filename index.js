5a4cc4ca56c789cf2b3f6251f1c6e079f646c7ca
Cameron <camcreel@gmail.com>
2021-04-10 21:14:31 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+
+
+
+function submitData(userName,userEmail) {
+    const data = {
+        name: userName,
+        email: userEmail
+    };
+
+    const configObj = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+          },
+        body: JSON.stringify(data)
+
+    };
+    return fetch("http://localhost:3000/users", configObj)
+        .then(response => response.json())
+        .then(response => document.body.append(response.id))
+        .catch(error => document.body.append(error.message))
+    
+};
+
